26faabc41d07dbbc57b6a8a362233414f8376a4e
ivakavankova <kavankova.iva@gmail.com>
2018-03-20 22:46:54 +0100

    + data

--- 02.sql
+++ 02.sql
@@ -110,21 +110,41 @@ ALTER TABLE "rating"
 -- data pro dotazy
 INSERT INTO "supplier" ("name", "phone", "email") VALUES ('OLC Systems', '+420123456789', 'mail@olc.cz');
 INSERT INTO "supplier" ("name", "phone", "email") VALUES ('Sony corp', '+236874521369', 'sony@email.cz');
-INSERT INTO "supplier" ("name", "phone", "email") VALUES ('ASUS tech', '852369854', 'sony@email.cz');
+INSERT INTO "supplier" ("name", "phone", "email") VALUES ('ASUS tech', '852369854', 'asus@asus.com');
+INSERT INTO "supplier" ("name", "phone", "email") VALUES ('Papirnictvi Litovel', '777256958', 'papir@litovel.cz');
+INSERT INTO "supplier" ("name", "phone", "email") VALUES ('Potreby pro malire Prerov', '733256999', 'malirskepotreby@prerov.cz');
+INSERT INTO "supplier" ("name", "phone", "email") VALUES ('TESCO', '570248596', 'order@tesco.com');
+INSERT INTO "supplier" ("name", "phone", "email") VALUES ('Svet pastele', '720185469', 'pastelky@pastelky.cz');
 
 INSERT INTO "product" ("name", "price", "supplier_id") VALUES ('Dlouhe pastelky', 150, 1);
 INSERT INTO "product" ("name", "price", "supplier_id") VALUES ('Kratsi pastelky', 120, 1);
-INSERT INTO "product" ("name", "price", "supplier_id") VALUES ('DELUXE pastelky', 200, 1);
+INSERT INTO "product" ("name", "price", "supplier_id") VALUES ('Modre pastelky', 200, 2);
+INSERT INTO "product" ("name", "price", "supplier_id") VALUES ('Zelene pastelky', 202, 2);
+INSERT INTO "product" ("name", "price", "supplier_id") VALUES ('Cervene pastelky', 205, 3);
+INSERT INTO "product" ("name", "price", "supplier_id") VALUES ('Cerne pastelky', 208, 3);
+INSERT INTO "product" ("name", "price", "supplier_id") VALUES ('Zlute pastelky', 220, 4);
+INSERT INTO "product" ("name", "price", "supplier_id") VALUES ('Ruzove pastelky', 207, 4);
+INSERT INTO "product" ("name", "price", "supplier_id") VALUES ('Fialove pastelky', 200, 5);
+INSERT INTO "product" ("name", "price", "supplier_id") VALUES ('DELUXE pastelky', 205, 5);
+INSERT INTO "product" ("name", "price", "supplier_id") VALUES ('Pastelky super', 238, 6);
+INSERT INTO "product" ("name", "price", "supplier_id") VALUES ('Pastelky dobre', 100, 6);
+INSERT INTO "product" ("name", "price", "supplier_id") VALUES ('Tluste pastelky', 152, 7);
+INSERT INTO "product" ("name", "price", "supplier_id") VALUES ('Tenke pastelky', 170, 7);
 
 INSERT INTO "crayon" ("product_id", "type", "quantity", "length") VALUES (1, 1, 15, 25);
 INSERT INTO "crayon" ("product_id", "type", "quantity", "length") VALUES (2, 2, 10, 10);
 INSERT INTO "crayon" ("product_id", "type", "quantity", "length") VALUES (3, 1, 10, 15);
-
-INSERT INTO "product" ("name", "price", "supplier_id") VALUES ('Pastelky super', 238, 2);
-INSERT INTO "product" ("name", "price", "supplier_id") VALUES ('Pastelky dobre', 100, 2);
-
 INSERT INTO "crayon" ("product_id", "type", "quantity", "length") VALUES (4, 1, 2, 30);
 INSERT INTO "crayon" ("product_id", "type", "quantity", "length") VALUES (5, 2, 4, 18);
+INSERT INTO "crayon" ("product_id", "type", "quantity", "length") VALUES (6, 1, 32, 12);
+INSERT INTO "crayon" ("product_id", "type", "quantity", "length") VALUES (7, 2, 18, 8);
+INSERT INTO "crayon" ("product_id", "type", "quantity", "length") VALUES (8, 1, 5, 15);
+INSERT INTO "crayon" ("product_id", "type", "quantity", "length") VALUES (9, 2, 1, 21);
+INSERT INTO "crayon" ("product_id", "type", "quantity", "length") VALUES (10, 1, 25, 22);
+INSERT INTO "crayon" ("product_id", "type", "quantity", "length") VALUES (11, 2, 36, 23);
+INSERT INTO "crayon" ("product_id", "type", "quantity", "length") VALUES (12, 1, 42, 32);
+INSERT INTO "crayon" ("product_id", "type", "quantity", "length") VALUES (13, 1, 19, 42);
+INSERT INTO "crayon" ("product_id", "type", "quantity", "length") VALUES (14, 2, 30, 28);
 
 INSERT INTO "product" ("name", "price", "supplier_id") VALUES ('Skicaky velke', 500, 2);
 INSERT INTO "product" ("name", "price", "supplier_id") VALUES ('Skicaky vetsi', 560, 2);
@@ -198,5 +218,3 @@ INSERT INTO "order_item" ("order_id", "order", "quantity", "product_id") VALUES 
 INSERT INTO "order_item" ("order_id", "order", "quantity", "product_id") VALUES (7, 2, 7, 3);
 INSERT INTO "order_item" ("order_id", "order", "quantity", "product_id") VALUES (7, 3, 3, 4);
 INSERT INTO "order_item" ("order_id", "order", "quantity", "product_id") VALUES (7, 4, 10, 11);
-
-
